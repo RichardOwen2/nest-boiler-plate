ca723d48518f82b1812b1f364d3a3dba2755d89c
Richard Owen <richardowen2411@gmail.com>
2024-09-06 10:09:18 +0800

    feat: apply api token

--- src/middlewares/api-key.middleware.ts
+++ src/middlewares/api-key.middleware.ts
@@ -3,16 +3,16 @@ import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    const apiToken = req.headers['X-API-KEY'];
+    const apiToken = req.headers['x-api-token'];
 
     const validToken = process.env.API_TOKEN;
 
     if (!apiToken) {
-      throw new HttpException('API token is required', 401);
+      throw new HttpException('API TOKEN is required', 401);
     }
 
     if (apiToken !== validToken) {
-      throw new HttpException('Invalid API token', 401);
+      throw new HttpException('Invalid API TOKEN', 401);
     }
 
     next();
--- src/modules/notes/notes.module.ts
+++ src/modules/notes/notes.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
+import { ApiKeyMiddleware } from 'src/middlewares/api-key.middleware';
 
 @Module({
   imports: [PrismaModule],
   controllers: [NotesController],
   providers: [NotesService]
 })
-export class NotesModule {}
+export class NotesModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes('notes');
+  }
+}
